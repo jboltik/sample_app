331352f258e2e7e453f9aaafee5a3e0e458f27d5
Jacqueline Boltik <jboltik@gmail.com>
2014-08-24 13:08:15 -0400

    Finish user edit, update, index, and destroy actions has failed tests but app works

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-before_save { |user| user.email = user.email.downcase }
-before_save :create_remember_token 
+  before_save { |user| user.email = user.email.downcase }
+  before_create :create_remember_token 
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,6 +13,7 @@ before_save :create_remember_token
   validates :password_confirmation, presence: true
 
   private
+
   	def create_remember_token
   		self.remember_token = SecureRandom.urlsafe_base64
   	end
